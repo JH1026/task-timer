914b3842aa82d74d1d80351b48102dd5ddde4ce4
HalJune1026 <jh.dev.acc.1026.i2k9@gmail.com>
2023-04-12 21:07:45 +0900

    feat: add Router about tasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import CommonLayout from './features/generic/CommonLayout'
 import TaskLayout from './features/generic/TaskLayout'
 import TaskConfigPage from './pages/TaskConfigPage'
+import TaskRecordPage from './pages/TaskRecordPage'
+import TaskStatsPage from './pages/TaskStatsPage'
 import TopPage from './pages/TopPage'
 import { initStorage } from './utils/storage'
 
@@ -18,8 +20,8 @@ function App() {
         </Route>
         <Route element={<TaskLayout />} >
           <Route path="/task-config" element={<TaskConfigPage />} />
-          <Route path="/task-record" element={<TopPage />} />
-          <Route path="/task-stats" element={<TopPage />} />
+          <Route path="/task-record" element={<TaskRecordPage />} />
+          <Route path="/task-stats" element={<TaskStatsPage />} />
         </Route>
       </Routes>
     </HashRouter>
